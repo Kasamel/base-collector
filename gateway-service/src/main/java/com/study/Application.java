a15a34652977b823f91114ced4e7eb053d549a75
Михаил <bulanev2012@yandex.ru>
2019-09-02 23:15:06 +0300

    create projects

---
+++ gateway-service/src/main/java/com/study/Application.java
@@ -0,0 +1,17 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
